1bb32f79afb42f73ebfc09282a748077f8d25fac
Ly <ly.pham@ahead.com>
2022-10-28 20:01:24 -0700

    Added controller tests

---
+++ src/test/java/com/example/springboot/controllers/DoctorControllerTest.java
@@ -0,0 +1,54 @@
+package com.example.springboot.controllers;
+
+import com.example.springboot.model.Doctor;
+import com.example.springboot.repository.DoctorRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DoctorControllerTest {
+    @Autowired
+    DoctorRepository doctorRepository;
+    @Autowired
+    DoctorController doctorController;
+
+    @BeforeEach
+    void setUp() {
+        doctorRepository.deleteAll();
+    }
+
+    @Test
+    void createDoctor() {
+        Doctor testDoc = new Doctor("P", "Lee");
+        Doctor tempDoc = doctorController.createDoctor(testDoc);
+        assertTrue(doctorRepository.findById(tempDoc.getId()).isPresent());
+        assertEquals(testDoc.getFirstName(), tempDoc.getFirstName());
+        assertEquals(testDoc.getLastName(), tempDoc.getLastName());
+    }
+
+    @Test
+    void getAllDoctors() {
+        assertTrue(doctorController.getAllDoctors().isEmpty());
+        Doctor testDoc = new Doctor("P", "Lee");
+        doctorRepository.save(testDoc);
+        assertFalse(doctorController.getAllDoctors().isEmpty());
+        Doctor testDoc2 = new Doctor("g", "da");
+        doctorRepository.save(testDoc2);
+        assertEquals(2, doctorController.getAllDoctors().size());
+    }
+
+    @Test
+    void getSpecificDoctor() {
+        Doctor testDoc = new Doctor("P", "Lee");
+        doctorRepository.save(testDoc);
+        Optional<Doctor> tempDoc = doctorController.getSpecificDoctor(testDoc.getId());
+        assertTrue(tempDoc.isPresent());
+        assertEquals(testDoc, tempDoc.get());
+    }
+}
